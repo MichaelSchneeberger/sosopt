392f27b757b42d20f4a9de0c2f8b503f61bb69ae
Michael Schneeberger <michael.schneeberger@fhnw.ch>
2024-08-27 10:51:20 +0200

    Retrieve variable symbols using indices from the to_variable_indices polymat operator.

--- sosopt/constraints/decisionvariablesmixin.py
+++ sosopt/constraints/decisionvariablesmixin.py
@@ -19,12 +19,22 @@ class DecisionVariablesMixin(ABC):
 def to_decision_variable_symbols(expr: MatrixExpression):
     @do()
     def _to_decision_variable_symbols():
-        variables = yield from polymat.to_variables(expr)
+        # variables = yield from polymat.to_variables(expr)
+
+        variable_indices = yield from polymat.to_variable_indices(expr)
+
+        state = yield from statemonad.get[State]()
+
+        def gen_polynomial_indices():
+            for index in variable_indices:
+                yield state.get_symbol(index=index)
+
+        symbols = tuple(set(gen_polynomial_indices()))
 
         def gen_decision_variable_symbols():
-            for variable in variables:
-                if isinstance(variable, DecisionVariableSymbol):
-                    yield variable
+            for symbol in symbols:
+                if isinstance(symbol, DecisionVariableSymbol):
+                    yield symbol
 
         decision_variable_symbols = tuple(gen_decision_variable_symbols())
 
--- sosopt/constraints/polynomialvariablesmixin.py
+++ sosopt/constraints/polynomialvariablesmixin.py
@@ -41,9 +41,9 @@ def to_polynomial_variables(
                 if not isinstance(symbol, DecisionVariableSymbol):
                     yield index
 
-        indices = tuple(gen_polynomial_indices())
+        polynomial_indices = tuple(gen_polynomial_indices())
 
-        vector = polymat.from_variable_indices(indices).cache()
+        vector = polymat.from_variable_indices(polynomial_indices).cache()
 
         # value = yield from polymat.to_sympy(vector)
         # print(f'{value=}')
@@ -51,42 +51,3 @@ def to_polynomial_variables(
         return statemonad.from_[State](vector)
 
     return _to_polynomial_variables()
-
-
-
-# def to_polynomial_variable(
-#     condition: PolynomialExpression,
-# ) -> StateMonad[State, VariableVectorExpression]:
-#     """ Assume everything that is not a decision variable to be a polynomial variable """
-
-#     @do()
-#     def filter_polynomial_variable_indices():
-
-#         # collect all variables appearing in the expression
-#         variable_vector = condition.to_variable_vector()
-
-#         # get indices in the same order as they appear in the variable vector
-#         variable_indices = yield from polymat.to_variable_indices(condition)
-
-#         # print(f'{variable_indices=}')
-#         # print('done filtering')
-#         state = yield from statemonad.get[State]()
-
-#         def gen_predicator():
-#             for index in variable_indices:
-#                 variable = state.get_variable(index=index)
-
-#                 # print(f'{variable=}')
-#                 is_poly_variable = not isinstance(variable, DecisionVariable)
-#                 yield is_poly_variable
-
-#         predicator = tuple(gen_predicator())
-#         # print(f'{predicator=}')
-#         filtered_vector = variable_vector.filter(predicator=predicator).cache()
-
-#         value = yield from polymat.to_sympy(filtered_vector)
-#         print(f'{value=}')
-
-#         return statemonad.from_[State](filtered_vector)
-
-#     return filter_polynomial_variable_indices()
