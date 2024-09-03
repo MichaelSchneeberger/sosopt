fb161962bfe21cca10acfbf0c66b7bd8417a0895
Michael Schneeberger <michael.schneeberger@fhnw.ch>
2024-09-03 17:19:45 +0200

    Remove to_gram_matrix function from readmeexample.py.

--- examples/readmeexample.py
+++ examples/readmeexample.py
@@ -19,20 +19,20 @@ w2 = ((x1 + 0.3) / 20) ** 2 + (x2 / 1.3) ** 2 + (x3 / 1.3) ** 2 - 1
 
 # Define a polynomial variables whose coefficients are all decision variables
 # of the resulting SOS problem.
-roi_poly_var = sosopt.define_polynomial(
-    name='roi',
+r_var = sosopt.define_polynomial(
+    name='r',
     monomials=x.combinations(degrees=(1, 2)),
     polynomial_variables=x,
 )
 # Fix the constant part of the polynomial to 1 to ensure numerical stability.
-roi = roi_poly_var - 1
+r = r_var - 1
 
 # Prints the symbol representation of the polynomial variable
-# roi_poly_var = 
-#   roi_0*x_1 + roi_1*x_2 + roi_2*x_3 + roi_3*x_1**2 + roi_4*x_1*x_2 
-#   + roi_5*x_1*x_3 + roi_6*x_2**2 + roi_7*x_2*x_3 + roi_8*x_3**2 - 1
-state, sympy_repr = polymat.to_sympy(roi).apply(state)
-print(f'roi={sympy_repr}')
+# r_poly_var = 
+#   r_0*x_1 + r_1*x_2 + r_2*x_3 + r_3*x_1**2 + r_4*x_1*x_2 
+#   + r_5*x_1*x_3 + r_6*x_2**2 + r_7*x_2*x_3 + r_8*x_3**2 - 1
+state, sympy_repr = polymat.to_sympy(r).apply(state)
+print(f'r={sympy_repr}')
 
 # use the do notation to avoid calling `apply(state)` method
 @do()
@@ -40,10 +40,10 @@ def define_sos_problem():
 
     # Use Putinar's Positivstellensatz to ensure the containment of
     # the cylindrical constraints encoded by w1 and w2 within the zero
-    # sublevel set of roi.
+    # sublevel set of r.
     constraint = yield from sosopt.sos_constraint_putinar(
-        name="roi",
-        less_than_zero=roi,
+        name="rpos",
+        less_than_zero=r,
         domain=sosopt.set_(
             less_than_zero={
                 "w1": w1,
@@ -52,12 +52,12 @@ def define_sos_problem():
         ),
     )
 
-    # Minimize a surrogate of the volume of the zero sublevel set of roi.
-    roi_diag = sosopt.to_gram_matrix(roi, x).diag()
+    # Minimize a surrogate of the volume of the zero sublevel set of r.
+    r_diag = r.quadratic_in(x).diag()
 
     problem = sosopt.sos_problem(
-        lin_cost=-roi_diag.sum(),
-        quad_cost=roi_diag,
+        lin_cost=-r_diag.sum(),
+        quad_cost=r_diag,
         constraints=(constraint,),
         solver=sosopt.cvx_opt_solver,   # choose solver
         # solver=sosopt.mosek_solver,
@@ -73,9 +73,9 @@ state, sos_result = problem.solve().apply(state)
 
 # Prints a dictionary that maps each symbol to its corresponding value found by the solver:
 # sos_result.symbol_values={
-#   'roi': (0.5442930462735407, 7.960826937815216e-28, 1.5901672575501424e-25, ...), 
-#   'roi_w1_gamma': (0.22625415065548013,), 
-#   'roi_w2_gamma': (0.8553898002589362,)}
+#   'r': (0.5442930462735407, 7.960826937815216e-28, 1.5901672575501424e-25, ...), 
+#   'r_w1_gamma': (0.22625415065548013,), 
+#   'r_w2_gamma': (0.8553898002589362,)}
 print(f'{sos_result.symbol_values=}')
 
 print(f'{sos_result.solver_data.status}')      # Output will be 'optimal'
