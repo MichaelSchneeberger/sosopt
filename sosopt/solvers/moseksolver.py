4ab817e3116da32be219610907d4439d48f2ef15
Michael Schneeberger <michael.schneeberger@fhnw.ch>
2024-10-10 17:21:30 +0200

    Bug fix: Off diagonal elements of vectrized semidefinite matrix must be scaled by sqrt(2) when using the Mosek solver.

--- sosopt/solvers/moseksolver.py
+++ sosopt/solvers/moseksolver.py
@@ -31,52 +31,49 @@ class MosekSolver(SolverMixin):
         def to_quadratic_size(n):
             n_sqrt = np.sqrt(n)
 
-            assert n_sqrt.is_integer(), f'{n_sqrt=}'
+            assert n_sqrt.is_integer(), f'{n=}'
 
             return int(n_sqrt)
 
-        def to_vectorized_tril_indices(n_col):
+        def to_vectorized_tril_indices(n_col, offset=0):
             """
             The row indices for a 2x2 matrix are [0, 2, 3].
             """
             size = to_quadratic_size(n_col)
-            row, col = np.tril_indices(size)
+            row, col = np.tril_indices(size, offset)
             return sorted(np.ravel_multi_index((col, row), (size, size)))
+        
+        def to_sparse_representation(G):
+            afeidx, varidx = np.nonzero(G)
+            f_val = G[afeidx, varidx]
+            return tuple(afeidx), tuple(varidx), tuple(f_val)
         
         def gen_s_arrays():
             for array in info.s_data:
                 row_indices = to_vectorized_tril_indices(array.n_eq)
+                off_diag_indices = to_vectorized_tril_indices(array.n_eq, -1)
+
+                array[0][off_diag_indices,:] *= np.sqrt(2)
+                array[1][off_diag_indices,:] *= np.sqrt(2)
 
                 # Mosek requires only the lower-triangle entries of the semi-definite matrix
                 yield array[0][row_indices, :], array[1][row_indices, :]
-        
+
         s_arrays = tuple(gen_s_arrays())
 
-        q = info.lin_cost[1].T
         h = np.vstack(tuple(c[0] for c in s_arrays))
         G = np.vstack(tuple(c[1] for c in s_arrays))
 
         n_eq = G.shape[0]
         n_var = G.shape[1]
 
-        def to_sparse_representation(G):
-            afeidx, varidx = np.nonzero(G)
-            f_val = G[afeidx, varidx]
-            return tuple(afeidx), tuple(varidx), tuple(f_val)
-
         G_rows, G_vars, G_vals = to_sparse_representation(G)
-
-        if info.eq_data:
-            b = np.vstack(tuple(c[0] for c in info.eq_data))
-            A = np.vstack(tuple(-c[1] for c in info.eq_data))
-
-            A_rows, A_vars, A_vals = to_sparse_representation(A)
 
         with mosek.Task() as task:
             task.appendvars(n_var)
-            task.appendafes(n_eq)
 
             # linear cost
+            q = info.lin_cost[1].T
             for j in np.nonzero(q)[0]:
                 task.putcj(j, q[j, 0])
 
@@ -86,19 +83,23 @@ class MosekSolver(SolverMixin):
                 task.putvarbound(j, mosek.boundkey.fr, -inf, +inf)
     
             # add semi-definite entries
+            task.appendafes(n_eq)
             task.putafefentrylist(G_rows, G_vars, G_vals)
             task.putafegslice(0, n_eq, tuple(h))
 
-            index = 0
-
             # indicate which semi-definite entries belong to which semi-definite constraint
+            index = 0
             for array in s_arrays:
                 n_array_eq = array[0].shape[0]
-                task.appendacc(task.appendsvecpsdconedomain(n_array_eq), list(range(index, index+n_array_eq)), None)
+                task.appendacc(task.appendsvecpsdconedomain(n_array_eq), list(range(index, index + n_array_eq)), None)
                 index = index + n_array_eq
 
             if info.eq_data:
+                b = np.vstack(tuple(c[0] for c in info.eq_data))
+                A = np.vstack(tuple(-c[1] for c in info.eq_data))
                 n_lin_eq = A.shape[0]
+
+                A_rows, A_vars, A_vals = to_sparse_representation(A)
 
                 task.appendcons(n_lin_eq)
                 task.putaijlist(A_rows, A_vars, A_vals)
